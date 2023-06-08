b274dd2c5e5bde62db7193a704756d15452f432c
Arthur Vitor <109088305+dinnaky@users.noreply.github.com>
2023-06-08 13:59:27 -0300

    Add files via upload

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 16;
+const primeiroNome = "Arthur";
+const sobrenome = "Vitor";
+
+//console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0
+contador = contador +1
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto =2;
---
+++ teste.js
@@ -0,0 +1,14 @@
+//Teste informações
+
+const primeiroNome = "Arthur"
+const sobrenome = "Vitor"
+const nomeCompleto = primeiroNome + sobrenome
+const idade = "16"
+const dataNascimento = "03/07/2007"
+
+console.log ("INFORMAÇÕES")
+
+console.log (`Meu nome é ${primeiroNome}`)
+console.log (`Completo é ${nomeCompleto}`)
+console.log (`Tenho ${idade} anos`)
+console.log (`Nasci ${dataNascimento}`)
